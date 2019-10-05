04ab8b5492cfb513e22775db62885dd85adc2c94
Blaine Moser <blainemoser@outlook.com>
2019-10-06 01:02:06 +0200

    Added configs parser function

--- main.go
+++ main.go
@@ -4,24 +4,77 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"regexp"
+	"strings"
 	"time"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
-func main() {
-	// Find configs in the provided config file
-	configFile, err := os.OpenFile("confg.txt", os.O_RDONLY, 0700)
+// Checks whether a fatal error has been encountered
+func checkFatalErr(err error) {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+type configs struct {
+	urls            map[int]string
+	databaseConfigs map[string]string
+}
+
+func getConfigs() configs {
+	configFile, err := os.OpenFile("config.txt", os.O_RDONLY, 0700)
+	checkFatalErr(err)
 
 	// Get the configs from the file
+	configFileUnmarshalled := make([]byte, 1024)
+	configFileRead, err := configFile.Read(configFileUnmarshalled)
+	checkFatalErr(err)
 
-	// Close the file
-	if closeErr := configFile.Close(); closeErr != nil {
-		log.Fatal(closeErr)
+	fileContents := string(configFileUnmarshalled[:configFileRead])
+
+	splitter := regexp.MustCompile(`\n`) // splits the file contents by each new line
+	lines := splitter.Split(fileContents, -1)
+	var result configs
+	result.urls = make(map[int]string)
+	result.databaseConfigs = make(map[string]string)
+	countUrls := 0 // there can be multiple urls
+	for _, line := range lines {
+		configSplitter := regexp.MustCompile(`:`) // looks for colon to denote config value
+
+		commentIndex := strings.Index(line, "#") // comments use "#" indicators in config files
+		if commentIndex < 0 {
+			commentIndex = len(line)
+		}
+		config := strings.TrimSpace(line[0:commentIndex])
+		keyValPair := configSplitter.Split(config, 2)
+		if len(keyValPair) != 2 {
+			continue
+		}
+
+		if strings.ToLower(strings.TrimSpace(keyValPair[0])) == "url" {
+			result.urls[countUrls] = strings.TrimSpace(keyValPair[1])
+			countUrls++
+		} else {
+			result.databaseConfigs[strings.TrimSpace(keyValPair[0])] = strings.TrimSpace(keyValPair[1])
+		}
 	}
+	// Close the file
+	if err := configFile.Close(); err != nil {
+		log.Fatal(err)
+	}
+
+	return result
+}
+
+func main() {
+	// Find configs in the provided config file
+	// configData, err := ioutil.ReadFile("config.txt")
+	// checkFatalErr(err)
+
+	configs := getConfigs()
+	fmt.Println(configs)
 
 	// Run an indefinite update loop
 	for now := range time.Tick(time.Minute) {
